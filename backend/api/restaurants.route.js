816b72d3752504185fedd21455079e6460e97ee6
Nutthanich Narphromar <nutthanich.na@gmail.com>
2022-01-17 16:48:29 +0700

    Add routes for adding, updating, and deleting review

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,8 +1,15 @@
 import express from "express";
 import RestaurantsController from "./restaurants.controller.js";
+import ReviewController from "./reviews.controller.js";
 
 const router = express.Router();
 
 router.route("/").get(RestaurantsController.apiGetRestaurants);
+
+router
+  .route("/review")
+  .post(ReviewController.apiPostReview)
+  .put(ReviewController.apiUpdateReview)
+  .delete(ReviewController.apiDeleteReview)
 
 export default router
